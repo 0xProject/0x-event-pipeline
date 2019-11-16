0d25d32f1a424a8ca369f8ee76e0e79cfd0c3a3d
Alex Kroeger <alexrkroeger@gmail.com>
2019-11-16 12:36:22 -0800

    Added views as a migration, added scraping for the deployment time of the staking proxy contract

--- src/index.ts
+++ src/index.ts
@@ -9,13 +9,17 @@ import * as ormConfig from './ormconfig';
 import { SECONDS_BETWEEN_RUNS } from './config';
 
 import { EventScraper } from './scripts/pull_and_save_events';
+import { DeploymentScraper } from './scripts/pull_and_save_deployment';
 
 console.log("App is running...");
 
 const eventScraper = new EventScraper();
+const deploymentScraper = new DeploymentScraper();
 
 // run pull and save events
 createConnection(ormConfig as ConnectionOptions).then(async connection => {
+
+    await deploymentScraper.getParseSaveStakingProxyContractDeployment(connection);
 
     cron.schedule(`*/${SECONDS_BETWEEN_RUNS} * * * * *`, () => {
         eventScraper.getParseSaveEventsAsync(connection);
