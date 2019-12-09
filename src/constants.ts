9e78808e54037d4adb5e94f35101d37d144f0ef8
Alex Kroeger <alexrkroeger@gmail.com>
2019-12-09 12:19:37 -0800

    Made starting block and staking proxy deployment tx hash populate based on Chain ID

--- src/constants.ts
+++ src/constants.ts
@@ -1,12 +1,10 @@
 export const DEFAULT_LOCAL_POSTGRES_URI = 'postgresql://user:password@localhost/events';
-export const DEFAULT_FIRST_SEARCH_BLOCK = 8952139;
 export const DEFAULT_START_BLOCK_OFFSET = 35;
 export const DEFAULT_MAX_BLOCKS_TO_PULL = 100;
 export const DEFAULT_MAX_BLOCKS_TO_SEARCH = 100;
 export const DEFAULT_CHAIN_ID = 1;
 export const DEFAULT_BLOCK_FINALITY_THRESHOLD = 0;
 export const DEFAULT_SECONDS_BETWEEN_RUNS = 15;
-export const DEFAULT_STAKING_PROXY_DEPLOYMENT_TRANSACTION= '0x4680e9d59bae9bbde1b0bae0fa5157ceea64ea923f2be434e5da6f5df2bdb907';
 export const DEFAULT_STAKING_POOLS_JSON_URL = 'https://raw.githubusercontent.com/0xProject/0x-staking-pool-registry/master/staking_pools.json';
 export const DEFAULT_STAKING_POOLS_METADATA_JSON_URL = 'https://raw.githubusercontent.com/0xProject/0x-staking-pool-registry/master/pool_metadata.json';
 export const DEFAULT_BASE_GITHUB_LOGO_URL = 'https://github.com/0xProject/0x-staking-pool-registry/raw/master/logos/';
