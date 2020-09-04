fb670a25e16948eeabd8f3fe46d974b8995ba043
Alex Kroeger <alexrkroeger@gmail.com>
2020-09-04 17:03:02 -0400

    Added cancel event scraping

--- src/parsers/events/fill_events.ts
+++ src/parsers/events/fill_events.ts
@@ -1,11 +1,13 @@
 import { assetDataUtils } from '@0x/order-utils';
-import { AssetData, AssetProxyId, ERC20BridgeAssetData, MultiAssetData, MultiAssetDataWithRecursiveDecoding, StaticCallAssetData } from '@0x/types';
+import { AssetProxyId } from '@0x/types';
 import { LogWithDecodedArgs } from 'ethereum-types';
 import { ExchangeFillEventArgs } from '@0x/contract-wrappers';
 
 import { FillEvent } from '../../entities';
 import { convertAssetProxyIdToType } from '../../utils';
 import { parseEvent } from './parse_event';
+
+import { parse0xAssetTokenAddress, parseV30xBridgeAddress } from '../utils/asset_data_utils';
 
 
 /**
@@ -15,56 +17,6 @@ import { parseEvent } from './parse_event';
  */
 export function parseFillEvents(eventLogs: LogWithDecodedArgs<ExchangeFillEventArgs>[]): FillEvent[] {
     return eventLogs.map(fill => parseFillEvent(fill))
-}
-
-export function parse0xAssetTokenAddress(
-    decodedAssetData: AssetData):
-string | null {
-    if (isMultiAssetData(decodedAssetData)) {
-        return null;
-    } else if (isStaticCallAssetData(decodedAssetData)) {
-        return null;
-    } else {
-        return decodedAssetData.tokenAddress;
-    }
-}
-
-/**
- * Returns the bridge address if it's ERC20Bridge data
- * @param decodedAssetData decoded 0x asset data
- */
-export function parseV30xBridgeAddress(
-    decodedAssetData: AssetData):
-string | null {
-    if (isERC20BridgeAssetData(decodedAssetData)) {
-        return decodedAssetData.bridgeAddress;
-    } else {
-        return null;
-    }
-}
-
-/**
- * Checks if the asset data is ERC20Bridge Data
- * @param decodedAssetData decoded 0x asset data
- */
-export function isERC20BridgeAssetData(decodedAssetData: AssetData): decodedAssetData is ERC20BridgeAssetData {
-    return decodedAssetData.assetProxyId === AssetProxyId.ERC20Bridge;
-}
-
-/**
- * Checks if the asset data is multi-asset data
- * @param decodedAssetData decoded 0x asset data
- */
-export function isMultiAssetData(decodedAssetData: AssetData): decodedAssetData is MultiAssetData | MultiAssetDataWithRecursiveDecoding {
-    return decodedAssetData.assetProxyId === AssetProxyId.MultiAsset;
-}
-
-/**
- * Checks if the asset data is static call asset data
- * @param decodedAssetData decoded 0x asset data
- */
-export function isStaticCallAssetData(decodedAssetData: AssetData): decodedAssetData is StaticCallAssetData {
-    return decodedAssetData.assetProxyId === AssetProxyId.StaticCall;
 }
 
 
