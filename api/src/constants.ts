90771674549a8bf32b2dfd2b0b10ecb9a0f8d84b
xianny <xianny@gmail.com>
2021-03-05 16:25:23 -0800

    wip [not-working]: add docker

--- api/src/constants.ts
+++ api/src/constants.ts
@@ -3,13 +3,13 @@ export const API_PATH = '/staking';
 export const DEFAULT_CACHE_AGE_SECONDS = 10;
 
 // Http service config
-export const DEFAULT_HTTP_PORT = 3000;
-export const DEFAULT_HEALTHCHECK_HTTP_PORT = 3000;
+export const DEFAULT_HTTP_PORT = 4000;
+export const DEFAULT_HEALTHCHECK_HTTP_PORT = 4000;
 export const DEFAULT_HEALTHCHECK_PATH = '/healthz';
 export const DEFAULT_HTTP_KEEP_ALIVE_TIMEOUT = 76 * 1000;
 export const DEFAULT_HTTP_HEADERS_TIMEOUT = 77 * 1000;
 export const DEFAULT_ENABLE_PROMETHEUS_METRICS = true;
-export const DEFAULT_PROMETHEUS_PORT = 3000;
+export const DEFAULT_PROMETHEUS_PORT = 4000;
 export const DEFAULT_PROMETHEUS_PATH = '/metrics';
 
 // Logging
