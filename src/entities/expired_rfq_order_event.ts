5d6ce8d94f33f9252e1c5b889eb73ec83d70a4a4
Danning Sui <danningsui@DanningrkLaptop.attlocal.net>
2021-03-15 21:43:33 -0700

    add scraper for v4cancel and expiredRfqOrder event

---
+++ src/entities/expired_rfq_order_event.ts
@@ -0,0 +1,21 @@
+import { Column, Entity } from 'typeorm';
+import { BigNumber } from '@0x/utils';
+
+import { Event } from './event';
+import { bigNumberTransformer } from '../utils';
+
+
+// These events come directly from the Exchange contract and are fired for meta transactions
+@Entity({ name: 'expired_rfq_order_events', schema: 'events' })
+export class ExpiredRfqOrderEvent extends Event {
+
+    // The hash of the order that was cancelled.
+    @Column({ name: 'order_hash', type: 'varchar' })
+    public orderHash!: string;
+    // The address of the maker.
+    @Column({ name: 'maker', type: 'varchar' })
+    public maker!: string;
+
+    @Column({ name: 'expiry', type: 'numeric', transformer: bigNumberTransformer })
+    public expiry!: BigNumber;
+}
