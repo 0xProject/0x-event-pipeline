8db2548cad58eead7e2ccbd913baf6884bfc5bd3
Ben Lyaunzon <lyaunzon.b@gmail.com>
2021-04-08 09:50:00 -0700

    splitting out rewards into seperate query

--- staking-api/src/handlers.ts
+++ staking-api/src/handlers.ts
@@ -10,6 +10,7 @@ import {
     StakingEpochsResponse,
     StakingEpochsWithFeesResponse,
     StakingPoolResponse,
+    StakingPoolRewardsResponse,
     StakingPoolsResponse,
     StakingStatsResponse,
 } from './types';
@@ -25,9 +26,7 @@ export class Handlers {
     }
     public async getStakingPoolByIdAsync(req: express.Request, res: express.Response): Promise<void> {
         const poolId = req.params.id;
-        console.log('yo1');
         const pool = await this._db.getStakingPoolWithStatsAsync(poolId);
-        const epochRewards = await this._db.getStakingPoolEpochRewardsAsync(poolId);
         const allTimeStats = await this._db.getStakingPoolAllTimeRewardsAsync(poolId);
 
         const response: StakingPoolResponse = {
@@ -35,6 +34,18 @@ export class Handlers {
             stakingPool: {
                 ...pool,
                 allTimeStats,
+            },
+        };
+
+        res.status(HttpStatus.OK).send(response);
+    }
+    public async getStakingPoolRewardsByIdAsync(req: express.Request, res: express.Response): Promise<void> {
+        const poolId = req.params.id;
+        const epochRewards = await this._db.getStakingPoolEpochRewardsAsync(poolId);
+
+        const response: StakingPoolRewardsResponse = {
+            poolId,
+            stakingPoolRewards: {
                 epochRewards,
             },
         };
