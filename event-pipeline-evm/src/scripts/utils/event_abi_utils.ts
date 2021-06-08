f4db3ef2ce09aa1472a280846a9c305b51bd25b8
Jorge Pérez <jorge@0xproject.com>
2021-06-08 18:45:32 -0500

    EVM Add generic VIP swap

--- event-pipeline-evm/src/scripts/utils/event_abi_utils.ts
+++ event-pipeline-evm/src/scripts/utils/event_abi_utils.ts
@@ -10,7 +10,7 @@ const Web3Utils = require('web3-utils');
 
 export interface DeleteOptions {
     isDirectTrade?: boolean;
-    directProtocol?: string;
+    directProtocol?: string[];
     protocolVersion?: string;
     nativeOrderType?: string;
 }
@@ -53,7 +53,7 @@ export class PullAndSaveEventsByTopic {
             rawLogsArray.map(async rawLogs => {
                 const parsedLogs = rawLogs.logs.map((encodedLog: RawLogEntry) => parser(encodedLog));
 
-                if (eventName === 'PancakeVIPEvent' && parsedLogs.length > 0) {
+                if (eventName === 'VIPSwapEvent' && parsedLogs.length > 0) {
                     var contractCallToken0Array = [];
                     var contractCallToken1Array = [];
 
@@ -153,9 +153,9 @@ export class PullAndSaveEventsByTopic {
 
         let deleteQuery: string;
         if (deleteOptions.isDirectTrade && deleteOptions.directProtocol != undefined) {
-            deleteQuery = `DELETE FROM ${SCHEMA}.${tableName} WHERE block_number >= ${startBlock} AND block_number <= ${endBlock} AND direct_protocol = '${
-                deleteOptions.directProtocol
-            }'`;
+            deleteQuery = `DELETE FROM ${SCHEMA}.${tableName} WHERE block_number >= ${startBlock} AND block_number <= ${endBlock} AND direct_protocol IN ('${deleteOptions.directProtocol.join(
+                "','",
+            )}')`;
         } else {
             if (tableName === 'native_fills' && deleteOptions.protocolVersion != undefined) {
                 if (deleteOptions.protocolVersion === 'v4' && deleteOptions.nativeOrderType != undefined) {
