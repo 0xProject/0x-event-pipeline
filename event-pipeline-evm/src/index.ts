d01e71938feae7be314980b5726e79795e10dc86
Jorge Pérez <jorge@0xproject.com>
2021-08-02 10:47:55 -0500

    EVM Check Chain ID before starting

--- event-pipeline-evm/src/index.ts
+++ event-pipeline-evm/src/index.ts
@@ -6,19 +6,22 @@ config({ path: resolve(__dirname, '../../.env') });
 
 import { ConnectionOptions, createConnection } from 'typeorm';
 import * as ormConfig from './ormconfig';
-import { MINUTES_BETWEEN_RUNS } from './config';
+import { MINUTES_BETWEEN_RUNS, CHAIN_ID } from './config';
 
 import { EventScraper } from './scripts/pull_and_save_events';
 import { EventsByTopicScraper } from './scripts/pull_and_save_events_by_topic';
+import { ChainIdChecker } from './scripts/check_chain_id';
 
 console.log('App is running...');
 
+const chainIdChecker = new ChainIdChecker();
 const eventScraper = new EventScraper();
 const eventsByTopicScraper = new EventsByTopicScraper();
 
 // run pull and save events
 createConnection(ormConfig as ConnectionOptions)
     .then(async connection => {
+        await chainIdChecker.checkChainIdAsync(CHAIN_ID);
         cron.schedule(`*/${MINUTES_BETWEEN_RUNS} * * * *`, () => {
             Promise.all([
                 eventScraper.getParseSaveEventsAsync(connection),
