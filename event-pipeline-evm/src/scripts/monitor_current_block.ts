bdcbb2e9a5c69097f8ed9bdde223cbbc1d8ecaae
Jorge Pérez <jorge@0xproject.com>
2021-09-14 13:23:52 -0500

    Individual monitor for current block

---
+++ event-pipeline-evm/src/scripts/monitor_current_block.ts
@@ -0,0 +1,26 @@
+import { Web3Wrapper } from '@0x/web3-wrapper';
+import { web3Factory } from '@0x/dev-utils';
+import { logger } from '../utils/logger';
+import 'reflect-metadata';
+import { Gauge } from 'prom-client';
+
+import { CHAIN_NAME, ETHEREUM_RPC_URL } from '../config';
+
+const provider = web3Factory.getRpcProvider({
+    rpcUrl: ETHEREUM_RPC_URL,
+});
+
+export const CURRENT_BLOCK = new Gauge({
+    name: 'event_scraper_current_block',
+    help: 'The current head of the chain',
+    labelNames: ['chain'],
+});
+
+export class CurrentBlockMonitor {
+    public async monitor(): Promise<void> {
+        const web3Wrapper = new Web3Wrapper(provider);
+        const currentBlock = await web3Wrapper.getBlockNumberAsync();
+        CURRENT_BLOCK.labels({ chain: CHAIN_NAME }).set(currentBlock);
+        logger.info(`Current block: ${currentBlock}`);
+    }
+}
