b1e2f13b039fc16b1ed2f3f1729882a7ed00c753
Jorge Pérez <jorge@ktlxv.com>
2022-02-28 10:12:16 -0600

    Fix Uniswap V3 Parser

--- event-pipeline-evm/src/parsers/events/swap_events.ts
+++ event-pipeline-evm/src/parsers/events/swap_events.ts
@@ -44,11 +44,16 @@ export function parseUniswapV3SwapEvent(eventLog: RawLogEntry): ERC20BridgeTrans
     // decode the basic info directly into eRC20BridgeTransferEvent
     const decodedLog = abiCoder.decodeLog(SWAP_V3_ABI.inputs, eventLog.data, [eventLog.topics[1], eventLog.topics[2]]);
 
-    eRC20BridgeTransferEvent.fromToken = '1'; // taker_token
-    eRC20BridgeTransferEvent.toToken = '0'; // maker_token
+    const amount0 = new BigNumber(Math.abs(decodedLog.amount0));
+    const amount1 = new BigNumber(Math.abs(decodedLog.amount1));
 
-    eRC20BridgeTransferEvent.fromTokenAmount = new BigNumber(Math.abs(decodedLog.amount1)); // taker_token_amount
-    eRC20BridgeTransferEvent.toTokenAmount = new BigNumber(Math.abs(decodedLog.amount0)); // maker_token_amount
+    // amount0 and amount1 are of opposite signs
+    // neg value means token left the pool ie. maker
+    // pos value means token entered the pool ie. taker
+    eRC20BridgeTransferEvent.fromToken = decodedLog.amount0 >= 0 ? '0' : '1'; // taker_token
+    eRC20BridgeTransferEvent.toToken = decodedLog.amount0 < 0 ? '0' : '1'; // maker_token
+    eRC20BridgeTransferEvent.fromTokenAmount = decodedLog.amount0 >= 0 ? amount0 : amount1; // taker_token_amount
+    eRC20BridgeTransferEvent.toTokenAmount = decodedLog.amount0 < 0 ? amount0 : amount1; // maker_token_amount
     eRC20BridgeTransferEvent.from = 'UniswapV3'; // maker
     eRC20BridgeTransferEvent.to = decodedLog.recipient.toLowerCase(); // taker
     eRC20BridgeTransferEvent.directFlag = true;
