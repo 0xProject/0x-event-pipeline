ee5b1484c7e5086470178c529012ae43b9e13df6
Jorge Pérez <jorge@ktlxv.com>
2022-02-28 15:26:31 -0600

    Separate legacy events

--- event-pipeline-evm/src/index.ts
+++ event-pipeline-evm/src/index.ts
@@ -1,14 +1,14 @@
 // load env vars
 import { resolve } from 'path';
 import { config } from 'dotenv';
-import * as cron from 'node-cron';
 config({ path: resolve(__dirname, '../../.env') });
 
 import { ConnectionOptions, createConnection } from 'typeorm';
 import * as ormConfig from './ormconfig';
 import { CHAIN_ID, ENABLE_PROMETHEUS_METRICS, SECONDS_BETWEEN_RUNS } from './config';
 
-import { EventScraper } from './scripts/pull_and_save_events';
+import { LegacyEventScraper } from './scripts/pull_and_save_legacy_events';
+import { BlocksTxScraper } from './scripts/pull_and_save_blocks_and_tx';
 import { EventsByTopicScraper } from './scripts/pull_and_save_events_by_topic';
 import { ChainIdChecker } from './scripts/check_chain_id';
 import { CurrentBlockMonitor } from './scripts/monitor_current_block';
@@ -17,7 +17,8 @@ import { startMetricsServer } from './utils/metrics';
 console.log('App is running...');
 
 const chainIdChecker = new ChainIdChecker();
-const eventScraper = new EventScraper();
+const legacyEventScraper = new LegacyEventScraper();
+const blocksTxScraper = new BlocksTxScraper();
 const eventsByTopicScraper = new EventsByTopicScraper();
 const currentBlockMonitor = new CurrentBlockMonitor();
 
@@ -31,8 +32,11 @@ chainIdChecker.checkChainId(CHAIN_ID);
 createConnection(ormConfig as ConnectionOptions)
     .then(async (connection) => {
         schedule(null, currentBlockMonitor.monitor, 'Current Block');
-        schedule(connection, eventScraper.getParseSaveEventsAsync, 'Pull and Save Events');
+        schedule(connection, blocksTxScraper.getParseSaveEventsAsync, 'Pull and Save Blocks and Transactions');
         schedule(connection, eventsByTopicScraper.getParseSaveEventsAsync, 'Pull and Save Events by Topic');
+        if (CHAIN_ID === 1) {
+            schedule(connection, legacyEventScraper.getParseSaveEventsAsync, 'Pull and Save Legacy Events');
+        }
     })
     .catch((error) => console.log(error));
 
---
+++ event-pipeline-evm/src/scripts/pull_and_save_blocks_and_tx.ts
@@ -0,0 +1,49 @@
+import { web3Factory } from '@0x/dev-utils';
+import { Web3ProviderEngine } from '@0x/subproviders';
+import { logger } from '../utils/logger';
+import { Web3Wrapper } from '@0x/web3-wrapper';
+import 'reflect-metadata';
+import { Connection } from 'typeorm';
+
+import { PullAndSaveWeb3 } from './utils/web3_utils';
+import { Web3Source } from '../data_sources/events/web3';
+import { BLOCK_FINALITY_THRESHOLD, ETHEREUM_RPC_URL } from '../config';
+
+import { SCRIPT_RUN_DURATION } from '../utils/metrics';
+
+const provider = web3Factory.getRpcProvider({
+    rpcUrl: ETHEREUM_RPC_URL,
+});
+
+const web3Source = new Web3Source(provider, ETHEREUM_RPC_URL);
+const pullAndSaveWeb3 = new PullAndSaveWeb3(web3Source);
+
+export class BlocksTxScraper {
+    public async getParseSaveEventsAsync(connection: Connection): Promise<void> {
+        const startTime = new Date().getTime();
+        logger.info(`pulling events`);
+        const latestBlockWithOffset = await calculateEndBlockAsync(provider);
+
+        logger.info(`latest block with offset: ${latestBlockWithOffset}`);
+
+        const promises: Promise<void>[] = [];
+
+        promises.push(pullAndSaveWeb3.getParseSaveBlocks(connection, latestBlockWithOffset));
+        promises.push(pullAndSaveWeb3.getParseSaveTx(connection, latestBlockWithOffset));
+        promises.push(pullAndSaveWeb3.getParseSaveTxReceiptsAsync(connection, latestBlockWithOffset));
+
+        await Promise.all(promises);
+
+        const endTime = new Date().getTime();
+        const scriptDurationSeconds = (endTime - startTime) / 1000;
+        SCRIPT_RUN_DURATION.set({ script: 'events' }, scriptDurationSeconds);
+
+        logger.info(`Finished pulling blocks and tx in ${scriptDurationSeconds}`);
+    }
+}
+
+async function calculateEndBlockAsync(provider: Web3ProviderEngine): Promise<number> {
+    const web3Wrapper = new Web3Wrapper(provider);
+    const currentBlock = await web3Wrapper.getBlockNumberAsync();
+    return currentBlock - BLOCK_FINALITY_THRESHOLD;
+}
