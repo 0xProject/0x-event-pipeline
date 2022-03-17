f0a9c4b3d18be3ecd5cfba074c8677788cbcb28c
Jorge Pérez <jorge@ktlxv.com>
2022-03-17 16:47:18 -0600

    Scrape token metadata on all chains

---
+++ event-pipeline-evm/migrations/polygon/1647471375000-CreateTokenMetadataTable.ts
@@ -0,0 +1,32 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+const downQuery = `
+DROP TABLE events.token_metadata;
+`;
+
+const upQuery = `
+CREATE TABLE events.tokens_metadata (
+  observed_timestamp int8 NOT NULL,
+  address varchar NOT NULL,
+  "type" varchar NOT NULL,
+  symbol varchar NULL,
+  "name" varchar NULL,
+  decimals int8 NULL,
+  CONSTRAINT tokens_metadata_pk PRIMARY KEY (address)
+);
+CREATE INDEX tokens_metadata_type_idx ON events.tokens_metadata USING btree (type);
+
+INSERT INTO events.tokens_metadata (observed_timestamp,address,"type",symbol,"name","decimals")
+  VALUES (0,'0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee','Native','MATIC','Matic Token',18);
+
+`;
+
+export class CreateTokenMetadataTable1647471375000 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(upQuery);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(downQuery);
+    }
+}
