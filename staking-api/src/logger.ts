e275412fc8ed001ae4a6c20f2f854ccb5df75b1a
Jorge Pérez <jorge@0xproject.com>
2022-06-29 12:40:45 -0500

    Clean up deprecated code

--- staking-api/src/logger.ts
+++
@@ -1,9 +0,0 @@
-import * as pino from 'pino';
-
-import { LOGGER_INCLUDE_TIMESTAMP, LOG_LEVEL } from './config';
-
-export const logger = pino({
-    level: LOG_LEVEL,
-    useLevelLabels: true,
-    timestamp: LOGGER_INCLUDE_TIMESTAMP,
-});
