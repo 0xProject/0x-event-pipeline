e2bf0caa24e6c2b956e59f583f33261c133ae52d
Jorge Pérez <jorge@0xproject.com>
2023-02-23 18:01:42 -0600

    Add Scraping for Meta Transaction Executed Event

---
+++ src/entities/meta_transaction_executed_event.ts
@@ -0,0 +1,21 @@
+import { BigNumber } from '@0x/utils';
+import { Column, Entity } from 'typeorm';
+
+import { Event } from './event';
+import { bigNumberTransformer } from '../transformers';
+
+@Entity({ name: 'meta_transaction_executed_events' })
+export class MetaTransactionExecutedEvent extends Event {
+    // The meta-transaction hash
+    @Column({ name: 'hash', type: 'varchar' })
+    public hash!: string;
+    // The selector of the function being executed
+    @Column({ name: 'selector', type: 'varchar' })
+    public selector!: string;
+    // Who to execute the meta-transaction on behalf of
+    @Column({ name: 'signer', type: 'varchar' })
+    public signer!: string;
+    // Who executed the meta-transaction
+    @Column({ name: 'sender', type: 'varchar' })
+    public sender!: string;
+}
