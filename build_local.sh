8fd646d3f409c70e38492be8d3f3432430a50d0c
Yijing Yuan <yijing@0xproject.com>
2023-03-22 15:03:26 -0500

    small changes

--- build_local.sh
+++ build_local.sh
@@ -4,4 +4,4 @@ docker-compose -f docker-compose.yml up -d postgres
 docker-compose -f docker-compose.yml build event-pipeline-ethereum
 
 # `-f docker-compose.dev.yml` add environment variables for testing
-docker-compose -f docker-compose.yml -f docker-compose.dev.yml up event-pipeline-ethereum
+docker-compose -f docker-compose.yml up event-pipeline-ethereum
