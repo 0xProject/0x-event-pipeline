262ef24db39d86a6a679c418b86a99bcc79550a3
0xyijing <120416247+0xyijing@users.noreply.github.com>
2023-03-23 11:15:20 -0500

    Feat/fix null symbols (#122)
    * allow ASCII remove control characters
    
    * simple optimization
    
    * yarn fix
    
    * small changes
    
    * add service to docker-compose.yml
    
    * small changes
    
    * update env for token_scraper_ethreum

---
+++ build_local.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+docker-compose -f docker-compose.yml up -d postgres
+docker-compose -f docker-compose.yml build event-pipeline-ethereum
+
+# `-f docker-compose.dev.yml` add environment variables for testing
+docker-compose -f docker-compose.yml up event-pipeline-ethereum
