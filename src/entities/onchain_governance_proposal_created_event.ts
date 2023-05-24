7c7cdc4eab928feb0a67102ade5f0a7f03018bf3
0xyijing <120416247+0xyijing@users.noreply.github.com>
2023-05-24 13:05:26 -0500

    add 4 events for onchain governance (#127)
    * add 4 events for onchain governance
    
    * prettier
    
    * add empty line
    
    ---------
    
    Co-authored-by: Yijing <0xyijing@Yijings-MBP.1337.lan>

---
+++ src/entities/onchain_governance_proposal_created_event.ts
@@ -0,0 +1,27 @@
+import { BigNumber } from '@0x/utils';
+import { Column, Entity } from 'typeorm';
+
+import { Event } from './event';
+import { bigNumberTransformer } from '../transformers';
+
+@Entity({ name: 'onchain_governance_proposal_created' })
+export class OnchainGovernanceProposalCreatedEvent extends Event {
+    @Column({ name: 'proposal_id', type: 'bigint', transformer: bigNumberTransformer })
+    public proposal_id!: BigNumber;
+    @Column({ name: 'proposer', type: 'varchar' })
+    public proposer!: string;
+    @Column({ name: 'targets', type: 'varchar' })
+    public targets!: string;
+    @Column({ name: 'signatures', type: 'varchar' })
+    public signatures!: string;
+    @Column({ name: 'calldatas', type: 'varchar' })
+    public calldatas!: string;
+    @Column({ name: 'start_block', type: 'bigint', transformer: bigNumberTransformer })
+    public start_block!: BigNumber;
+    @Column({ name: 'end_block', type: 'bigint', transformer: bigNumberTransformer })
+    public end_block!: BigNumber;
+    @Column({ name: 'description', type: 'varchar' })
+    public description!: string;
+    @Column({ name: 'contract_name', type: 'varchar' })
+    public contract_name!: string;
+}
