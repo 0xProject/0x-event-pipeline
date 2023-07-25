7e3e53ec64642baa5419dbaee84ee3378b7a3ffd
Yijing <0xyijing@Yijings-MBP.1337.lan>
2023-07-25 11:49:32 -0500

    migrations

--- build_local.sh
+++ build_local.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
-docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d postgres
+docker-compose -f docker-compose.yml up -d postgres
 docker-compose -f docker-compose.yml build event-pipeline-ethereum
+docker-compose -f docker-compose.yml build event-pipeline-base
 
 # `-f docker-compose.dev.yml` add environment variables for testing
 docker-compose -f docker-compose.yml up event-pipeline-ethereum
