cbbe75ad5c93c553c334ebff8ba3a1e7d765a808
Andrés Elizondo <andres@0xproject.com>
2024-06-03 18:44:54 -0600

    Adds support for any non-standard ERC20 Transfer event.

--- src/parsers/events/erc20_transfer_events.ts
+++ src/parsers/events/erc20_transfer_events.ts
@@ -13,11 +13,8 @@ export function parseERC20TransferEvent(eventLog: LogEntry): ERC20TransferEvent 
 
     parseEvent(eventLog, eRC20TransferEvent);
 
-    if (eventLog.topics.length === 4 && eventLog.data === '0x') {
-        // ERC721 Transfer - 4 topics, no data
-        return null;
-    } else if (eventLog.topics.length === 3 && eventLog.data !== '0x') {
-        // Standard ERC20 Transfer - 3 topics, 32 bytes of data
+    if (eventLog.topics.length === 3 && eventLog.data !== '0x') {
+        // Standard ERC20 Transfer - 3 topics (2 indexed), 32 bytes of data (3 inputs)
         const decodedLog = abiCoder.decodeLog(
             STANDARD_ERC20_TRANSFER_ABI.inputs,
             eventLog.data,
@@ -27,6 +24,42 @@ export function parseERC20TransferEvent(eventLog: LogEntry): ERC20TransferEvent 
         eRC20TransferEvent.from = decodedLog.from.toLowerCase();
         eRC20TransferEvent.to = decodedLog.to.toLowerCase();
         eRC20TransferEvent.value = new BigNumber(decodedLog.value);
+    } else if (eventLog.topics.length <= 3) {
+        // Non-Standard ERC20 Transfer - Any number of topics or data (3 inputs)
+        var baseInputs = [...STANDARD_ERC20_TRANSFER_ABI.inputs];
+        baseInputs.forEach((inp, index) => {
+            baseInputs[index].indexed = false;
+            return baseInputs;
+        });
+
+        var wasDecoded = false;
+        var rowInputs = baseInputs.map((object) => ({ ...object }));
+        // First "succesful" decoding is considered correct
+        // The `from` and `to` might be wrong.
+        // Left-most inputs are assumed more likely to be indexed.
+        for (var i = -1; i < baseInputs.length - 1; i++) {
+            if (i >= 0) rowInputs[i].indexed = true;
+            for (var j = i; j < baseInputs.length; j++) {
+                const currInputs = rowInputs.map((object) => ({ ...object }));
+
+                if (j >= 0) currInputs[j].indexed = true;
+                try {
+                    const decodedLog = abiCoder.decodeLog(currInputs, eventLog.data, eventLog.topics.slice(1));
+                    // Assume addresses won't have first 7 characters be 0's.
+                    if (
+                        decodedLog.from.substring(0, 9) !== '0x0000000' &&
+                        decodedLog.to.substring(0, 9) !== '0x0000000'
+                    ) {
+                        wasDecoded = true;
+                        eRC20TransferEvent.from = decodedLog.from.toLowerCase();
+                        eRC20TransferEvent.to = decodedLog.to.toLowerCase();
+                        eRC20TransferEvent.value = new BigNumber(decodedLog.value);
+                    }
+                    break;
+                } catch (e: unknown) {}
+            }
+            if (wasDecoded) break;
+        }
     } else {
         return null;
     }
