8ed18d5b7d5e556db48292578ef382d7e9a8fdd7
Andrés Elizondo <andres@0xproject.com>
2024-06-21 18:32:28 -0600

    Adds auto-update when a Settler ERC721 Transfer event is detected.

--- src/parsers/events/settler_erc721_transfer_events.ts
+++ src/parsers/events/settler_erc721_transfer_events.ts
@@ -1,5 +1,6 @@
 import { SETTLER_ERC721_TRANSFER_ABI } from '../../constants';
 import { SettlerERC721TransferEvent } from '../../entities';
+import { SettlerContractSingleton, SettlerContract } from '../../settlerContractSingleton';
 import { parseEvent } from './parse_event';
 import { BigNumber } from '@0x/utils';
 import { LogEntry } from 'ethereum-types';
@@ -18,5 +19,16 @@ export function parseSettlerERC721TransferEvent(eventLog: LogEntry): SettlerERC7
     settlerERC721TransferEvent.from = decodedLog.from.toLowerCase();
     settlerERC721TransferEvent.to = decodedLog.to.toLowerCase();
     settlerERC721TransferEvent.token_id = decodedLog.tokenId;
+
+    const settlerContractSingleton = SettlerContractSingleton.getInstance();
+    const newContract: SettlerContract[] = [
+        {
+            address: settlerERC721TransferEvent.to,
+            startBlock: settlerERC721TransferEvent.blockNumber,
+            endBlock: null,
+        },
+    ];
+    settlerContractSingleton.addNewContracts(newContract);
+
     return settlerERC721TransferEvent;
 }
