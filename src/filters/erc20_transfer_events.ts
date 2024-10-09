5fa45e9680ec7994dc9a41c3c3acc31089f6fcbd
Yijing <yijing@0xproject.com>
2024-10-09 15:38:56 -0500

    test fix

--- src/filters/erc20_transfer_events.ts
+++ src/filters/erc20_transfer_events.ts
@@ -2,23 +2,41 @@ import { Web3Source } from '../data_sources/events/web3';
 import { Event, Transaction } from '../entities';
 import { getParseTxsAsync } from '../scripts/utils/web3_utils';
 
-export async function filterERC20TransferEventsGetContext(events: Event[], web3Source: Web3Source): Promise<Event[]> {
+export async function filterERC20TransferEventsGetContext(
+    events: Event[],
+    web3Source: Web3Source,
+    allowedTxnList?: Set<string>,
+): Promise<Event[]> {
     if (events.length > 0) {
         const txHashes = events.map((log: Event) => log.transactionHash);
-        const txData = await getParseTxsAsync(web3Source, txHashes);
-        const filteredTxsHashes = txData.parsedTxs
-            .filter((tx: Transaction) => tx.quoteId)
-            .map((tx: Transaction) => tx.transactionHash);
+        let validTxHashSet: Set<string>;
+        if (allowedTxnList) {
+            validTxHashSet = allowedTxnList;
+        } else {
+            const txData = await getParseTxsAsync(web3Source, txHashes);
+            const filteredTxsHashes = txData.parsedTxs
+                .filter((tx: Transaction) => tx.quoteId)
+                .map((tx: Transaction) => tx.transactionHash);
 
-        const validTxHashSet = new Set(filteredTxsHashes);
+            validTxHashSet = new Set(filteredTxsHashes);
+        }
+
         const filteredLogs = events.filter((log: Event) => validTxHashSet.has(log.transactionHash));
         return filteredLogs.filter((e) => e !== null);
     }
     return [];
 }
 
-export function filterERC20TransferEvents(events: Event[], transaction: Transaction): Event[] {
-    if (transaction.quoteId) {
+export function filterERC20TransferEvents(
+    events: Event[],
+    transaction: Transaction,
+    allowedTxnList?: Set<string>,
+): Event[] {
+    if (allowedTxnList) {
+        if (allowedTxnList.has(transaction.transactionHash)) {
+            return events.filter((e) => e !== null);
+        }
+    } else if (transaction.quoteId) {
         return events.filter((e) => e !== null);
     }
     return [];
