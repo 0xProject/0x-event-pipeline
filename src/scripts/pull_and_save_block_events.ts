5578514b3f5eeffbf12503b4cd120707604c8661
AndresElizondo <50649599+AndresElizondo@users.noreply.github.com>
2025-03-19 09:56:00 -0600

    Avoids edge case causing missing blocks due to backfilling blocks ahe… (#170)
    * Avoids edge case causing missing blocks due to backfilling blocks ahead of the event scraper.
    
    * Prettier.
    
    * Upper case SQL.
    
    ---------
    
    Co-authored-by: Andrés Elizondo <andres@0xproject.com>

--- src/scripts/pull_and_save_block_events.ts
+++ src/scripts/pull_and_save_block_events.ts
@@ -363,6 +363,7 @@ export class BlockEventsScraper {
             `SELECT DISTINCT block_number
              FROM ${SCHEMA}.tx_backfill
              WHERE done = false
+             AND block_number < (SELECT MAX(block_number) FROM ${SCHEMA}.blocks)
              ORDER BY block_number
              LIMIT ${MAX_BLOCKS_TO_PULL}`,
         );
